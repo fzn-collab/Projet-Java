18b3ca6673614c8af74348440ebfcf322770ea73
meryem chamali <meryem.chamlali01@gmail.com>
2026-06-04 15:28:41 +0100

    Update ProjectRepository.java

---
+++ backend/src/main/java/com/connectentrepreneurs/project/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.connectentrepreneurs.project.repository;
+
+import com.connectentrepreneurs.project.model.Project;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface ProjectRepository extends MongoRepository<Project, String> {
+
+    List<Project> findByOwnerId(String ownerId);
+    List<Project> findByStatut(String statut);
+    List<Project> findBySecteur(String secteur);
+    long countByStatut(String statut);
+}
